9d9c9129de5d6093e629307dd7532d4af9362198
unknown <ericmbuthia11@gmail.com>
2025-08-27 12:37:55 +0300

    Added # to the path link

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,9 @@ const App = () => (
       <HashRouter >
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/features" element={<Features />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/#about" element={<About />} />
+          <Route path="/#features" element={<Features />} />
+          <Route path="/#contact" element={<Contact />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
